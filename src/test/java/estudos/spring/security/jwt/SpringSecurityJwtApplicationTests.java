5d2fc840dc7832089031e1c35e891e79a326884c
Thiago Marques <lerreg22@gmail.com>
2025-05-27 17:11:01 -0300

    coomit inicial projeto para estudo de spring security com jwt

---
+++ src/test/java/estudos/spring/security/jwt/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package estudos.spring.security.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
